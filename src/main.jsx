d021f239ce78f51f9c198984013473f0ddb94ff5
gin-muhlis <ginxrpl07@gmail.com>
2024-10-12 16:49:17 +0700

    feat: membuat error page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css';
 import App from './components/pages/App'
+import NotFound from './components/pages/NotFound';
 
 const routes = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    errorElement: <NotFound />
   }
 ])
 
